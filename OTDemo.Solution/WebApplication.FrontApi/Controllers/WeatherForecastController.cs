13683c4f6bc8ff219110e10ac5d8483e01e8a703
bulat <farukshin@molot.ru>
2024-08-26 14:50:06 +0300

    *  добавил тег , удалил активит, надо показать как можно объединять все трассировки

--- OTDemo.Solution/WebApplication.FrontApi/Controllers/WeatherForecastController.cs
+++ OTDemo.Solution/WebApplication.FrontApi/Controllers/WeatherForecastController.cs
@@ -12,10 +12,9 @@ namespace WebApplication.FrontApi.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-
     private static readonly ActivitySource _activitySource =
         new ActivitySource(nameof(WeatherForecastController), "1.0.0");
-    
+
     private IWeatherApiService _weatherApiService;
     IRequestClient<GetAllCitiesRequest> _client;
     private readonly WeatherMetrics _metrics;
@@ -31,46 +30,40 @@ public class WeatherForecastController : ControllerBase
         _metrics = metrics;
     }
 
-    
+
     [HttpGet("GetAllFromBackEndApi")]
     public async Task<List<WeatherData>?> GetAll()
     {
-        using var activity = _activitySource.StartActivity(nameof(GetAll));
-        _metrics.SummaryRequestByCityCounter.Add(1,new KeyValuePair<string, object?>("city","all"));
+        _metrics.SummaryRequestByCityCounter.Add(1, new KeyValuePair<string, object?>("city", "all"));
         var result = await _weatherApiService.GetAllCitiesAsync();
         return result;
     }
-    
+
     [HttpGet("GetAllFromBackEndApiEventually")]
-    public async Task<List<WeatherData>?> GetAllEventually()
+    public async Task<List<WeatherData>?> GetAllEventually(string? orderId)
     {
-        using var activity = _activitySource.StartActivity(nameof(GetAllEventually));
-        _metrics.SummaryRequestByCityCounter.Add(1,new KeyValuePair<string, object?>("city","all"));
+        if (!string.IsNullOrEmpty(orderId))
+            Activity.Current.SetTag("orderId", orderId);
+
+        _metrics.SummaryRequestByCityCounter.Add(1, new KeyValuePair<string, object?>("city", "all"));
         var response = await _client.GetResponse<GetAllCitiesResponse>(new GetAllCitiesRequest());
         return response.Message.Data;
     }
-    
-    
-    
+
+
     [HttpGet("GetByCityFromBackEndApi")]
     public async Task<WeatherData?> GetByCity(string city)
     {
-        using var activity = _activitySource.StartActivity(nameof(GetByCity));
         try
         {
             WeatherData? result = await _weatherApiService.GetWeatherByCityAsync(city);
-            activity?.AddEvent(new ActivityEvent($"weather for {city} is ready"));
-            
-            _metrics.SummaryRequestByCityCounter.Add(1,new KeyValuePair<string, object?>("city",result?.City));
+            _metrics.SummaryRequestByCityCounter.Add(1, new KeyValuePair<string, object?>("city", result?.City));
             _metrics.SetWeather(result!);
-            
             return result;
         }
         catch (Exception)
         {
-            activity?.SetStatus(ActivityStatusCode.Error);
             return null;
         }
     }
-  
 }
